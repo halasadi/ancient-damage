976b8fec20d2b506f5afedc30443b62224f4ec75
Kushal K Dey <kshldey@gmail.com>
2016-08-11 08:49:49 -0500

    flash and sfa outputs and codes

--- R/lindo2016-factor-analysis.R
+++ R/lindo2016-factor-analysis.R
@@ -51,7 +51,7 @@ FactorGGBar(loadings = ll$l,
                               axis_label_size = 7,
                               axis_label_face = "bold"),
             panel=list(panel_rows=2,
-                       panel_title="Factor Loadings Bar plot",
+                       panel_title="greedy FLASH Factor Loadings Bar plot (with C to T)",
                        panel_title_fontsize=10,
                        panel_title_font=3))
 
@@ -76,7 +76,7 @@ FactorGGBar(loadings = ll2$l,
                              axis_label_size = 7,
                              axis_label_face = "bold"),
             panel=list(panel_rows=2,
-                       panel_title="Factor Loadings Bar plot",
+                       panel_title="pool FLASH Factor Loadings Bar plot (with C to T)",
                        panel_title_fontsize=10,
                        panel_title_font=3))
 
@@ -125,7 +125,7 @@ FactorGGBar(loadings = ll$l,
                              axis_label_size = 7,
                              axis_label_face = "bold"),
             panel=list(panel_rows=2,
-                       panel_title="Factor Loadings Bar plot",
+                       panel_title="greedy FLASH Factor Loadings Bar plot (without C to T)",
                        panel_title_fontsize=10,
                        panel_title_font=3))
 
@@ -150,6 +150,86 @@ FactorGGBar(loadings = ll2$l,
                              axis_label_size = 7,
                              axis_label_face = "bold"),
             panel=list(panel_rows=2,
-                       panel_title="Factor Loadings Bar plot",
+                       panel_title="pool FLASH Factor Loadings Bar plot (without C to T)",
+                       panel_title_fontsize=10,
+                       panel_title_font=3))
+
+################  SFA on Lindo 2016 data    ##################
+
+write.table(voom_signature_counts, file="../summary_data/sfa_input_voom_sig_with_CtoT_lindo2016.txt", 
+            row.names=FALSE,col.names=FALSE, quote=FALSE, sep="\t")
+
+write.table(voom_signature_counts_no_CtoT, file="../summary_data/sfa_input_voom_sig_without_CtoT_lindo2016.txt", 
+            row.names=FALSE,col.names=FALSE, quote=FALSE, sep="\t")
+
+# ./sfa_mac -gen ../../sfa_inputs/sfa_input_voom_sig_without_CtoT_lindo2016.txt -g 50 -n 1536 -k 5 -iter 500 -r 800 -mn -mg -o ../../sfa_outputs/Lindo2016/without_CtoT/voom_lindo_without_CtoT
+#  ./sfa_mac -gen ../../sfa_inputs/sfa_input_voom_sig_with_CtoT_lindo2016.txt -g 50 -n 1536 -k 5 -iter 500 -r 800 -mn -mg -o ../../sfa_outputs/Lindo2016/voom_lindo_with_CtoT
+
+###  Case 1 : with CtoT included #######################
+# Expected complete Log likelihood at iteration 500: 8047.85
+# Marginal log likelihood at iteration 500: -25484
+# Residual variance at iteration 500: 72.649
+# Residual sum of squares at iteration 500: 7227.68
+
+###   Case 2 : without C to T  ########################
+# Expected complete Log likelihood at iteration 500: 12159.9
+# Marginal log likelihood at iteration 500: -22889
+# Residual variance at iteration 500: 71.2059
+# Residual sum of squares at iteration 500: 6649.5
+
+ll_load <- read.table("../utilities/sfa_Lindo2016/with_CtoT/voom_lindo_with_CtoT_lambda.out")
+dim(ll_load)
+
+annotation <- data.frame(
+  sample_id = paste0("X", c(1:NROW(ll_load))),
+  label = factor(c(rep("Ancient",25), rep("Modern",25)))
+)
+
+rownames(ll_load) <- paste0("X", c(1:NROW(ll$l)));
+
+FactorGGBar(loadings = ll_load,
+            annotation = annotation,
+            palette = list("mid"="white", 
+                           "low"="red", 
+                           "high"="blue", 
+                           "midpoint"=0),
+            yaxis_label = "Population Type",
+            figure_title = " ",
+            axis_tick = list(axis_ticks_length = .1,
+                             axis_ticks_lwd_y = .1,
+                             axis_ticks_lwd_x = .1,
+                             axis_label_size = 7,
+                             axis_label_face = "bold"),
+            panel=list(panel_rows=2,
+                       panel_title="SFA Factor Loadings Bar plot (with C to T)",
+                       panel_title_fontsize=10,
+                       panel_title_font=3))
+
+
+ll2_load <- read.table("../utilities/sfa_Lindo2016/without_CtoT/voom_lindo_without_CtoT_lambda.out")
+dim(ll2_load)
+
+annotation <- data.frame(
+  sample_id = paste0("X", c(1:NROW(ll2_load))),
+  label = factor(c(rep("Ancient",25), rep("Modern",25)))
+)
+
+rownames(ll2_load) <- paste0("X", c(1:NROW(ll2_load)));
+
+FactorGGBar(loadings = ll2_load,
+            annotation = annotation,
+            palette = list("mid"="white", 
+                           "low"="red", 
+                           "high"="blue", 
+                           "midpoint"=0),
+            yaxis_label = "Population Type",
+            figure_title = " ",
+            axis_tick = list(axis_ticks_length = .1,
+                             axis_ticks_lwd_y = .1,
+                             axis_ticks_lwd_x = .1,
+                             axis_label_size = 7,
+                             axis_label_face = "bold"),
+            panel=list(panel_rows=2,
+                       panel_title="SFA Factor Loadings Bar plot (without C to T)",
                        panel_title_fontsize=10,
                        panel_title_font=3))
