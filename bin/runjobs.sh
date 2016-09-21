6fabbd472950c862ccd00d38d74963475b7bbd4b
Hussein Al-Asadi <halasadi@midway-login2.rcc.local>
2016-09-21 17:41:57 -0500

    added subset of csv files for 153 ancients

--- bin/runjobs.sh
+++ bin/runjobs.sh
@@ -1,7 +1,7 @@
 # run the pipeline !!!
 snakemake --snakefile Snakefile_mdw \
 	  --cluster-config cluster.json \
-	  -c "sbatch -t {cluster.time} --mem {cluster.mem} -o {cluster.out} -e {cluster.err} --partition=pi-mstephens" \
+	  -c "sbatch -t {cluster.time} --mem {cluster.mem} -o {cluster.out} -e {cluster.err}" \
 	  -j 80 \
 	  -p \
 	    $*
