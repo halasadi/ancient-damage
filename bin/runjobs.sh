e665506336accfdb1abbe944d38a30c988a0f316
Hussein Al-Asadi <halasadi@midway-login2.rcc.local>
2016-10-11 16:15:33 -0500

    new Snakefile

--- bin/runjobs.sh
+++ bin/runjobs.sh
@@ -1,5 +1,5 @@
 # run the pipeline !!!
-snakemake --snakefile Snakefile_mdw \
+snakemake --snakefile Snakefile \
 	  --cluster-config cluster.json \
 	  -c "sbatch -t {cluster.time} --mem {cluster.mem} -o {cluster.out} -e {cluster.err}" \
 	  -j 80 \
