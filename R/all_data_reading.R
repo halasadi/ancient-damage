e36e452fabb68aa21826fbeaf5cafda8fa27890f
Kushal K Dey <kshldey@gmail.com>
2017-01-18 10:42:05 -0600

    updated figures for the new version of aRchaic

--- R/all_data_reading.R
+++ R/all_data_reading.R
@@ -4,31 +4,37 @@
 
 library(aRchaic)
 
-dir <- "../data/Lindo2016ancients/";
+dir <- "../data/Lindo2016ancients_strand_flank/";
 out <- aggregate_bin_counts(dir, breaks = c(-1, seq(1,20,1), 25, 35))
 save(out,
-     file="../processed_data/lindo2016ancients-counts-table.rda")
+     file="../processed_data/lindo2016ancients-counts-table-strand-flank.rda")
 
 
-dir <- "../data/Lindo2016moderns/";
+dir <- "../data/Lindo2016moderns_strand_flank/";
 out <- aggregate_bin_counts(dir, breaks = c(-1, seq(1,20,1), 25, 35))
 save(out,
-     file="../processed_data/lindo2016moderns-counts-table.rda")
+     file="../processed_data/lindo2016moderns-counts-table-strand-flank.rda")
 
-dir <- "../data/Sardinia2017/";
-out <- aggregate_bin_counts(dir, breaks = c(-1, seq(1,20,1), 25, 35))
-
-save(out,
-     file="../processed_data/sardinia2017.rda")
-
-dir <- "../data/Sherpa_data/";
+dir <- "../data/Sardinia_data_strand_flank/";
 out <- aggregate_bin_counts(dir, breaks = c(-1, seq(1,20,1), 25, 35))
 
 save(out,
-     file="../processed_data/sherpa2017.rda")
+     file="../processed_data/sardinia2017-strand-flank.rda")
+
+dir <- "../data/Sherpa_data_strand_flank/";
+out <- aggregate_bin_counts(dir, breaks = c(-1, seq(1,20,1), 25, 35))
+
+save(out,
+     file="../processed_data/sherpa2017-strand-flank.rda")
+
+dir <- "../data/AnnaGosling2016_strand_flank//";
+out <- aggregate_bin_counts(dir, breaks = c(-1, seq(1,20,1), 25, 35))
+
+save(out,
+     file="../processed_data/annagosling2016-strand-flank.rda")
 
 
-dir <- "../data/1000Gmoderns/";
+dir <- "../data/1000Gmoderns_data_strand_flank/";
 out <- aggregate_bin_counts(dir, breaks = c(-1, seq(1,20,1), 25, 35))
 save(out,
      file="../processed_data/1000Gmoderns-counts-table.rda")
@@ -43,8 +49,8 @@ out <- aggregate_bin_counts(dir, breaks = c(-1, seq(1,20,1), 25, 35))
 save(out,
      file="../processed_data/RISE-counts-table.rda")
 
-dir <- "../data/HGDPmoderns/";
+dir <- "../data/HGDPmoderns_data_strand_flank/";
 out <- aggregate_bin_counts(dir, breaks = c(-1, seq(1,20,1), 25, 35))
 save(out,
-     file="../processed_data/HGDPmoderns-counts-table.rda")
+     file="../processed_data/HGDPmoderns-counts-table-strand-flank.rda")
 
